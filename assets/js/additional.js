01c7b32a784af8fee46b6a4c16878d291ecaf162
HaruSasaki <aiqusubaru@gmail.com>
2021-11-18 16:05:23 +0700

    Make share button works for all share button :v

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -1,5 +1,5 @@
 window.addEventListener('load', function() {
-    document.getElementById('btn-share').addEventListener('click', function() {
+    document.getElementsByClassName('btn-share').addEventListener('click', function() {
       navigator.share({
         title: 'Check out Euphony Dev. project',
         text: 'Check it out! is very cool!',
