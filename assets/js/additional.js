7001d23a0c85eb53f803a3938fd7e947754374b5
HaruSasaki <73406832+HaruSasaki@users.noreply.github.com>
2021-11-18 15:30:27 +0700

    Update additional.js

--- assets/js/additional.js
+++ assets/js/additional.js
@@ -1,8 +1,4 @@
 window.addEventListener('load', function() {
-    if(!navigator.share) {
-      document.querySelector('.share-container').innerHTML = 'Web Share API not supported in this browser';
-      return;
-    }
     document.getElementById('btn-share').addEventListener('click', function() {
       navigator.share({
         title: 'Check out Euphony Dev. project',
